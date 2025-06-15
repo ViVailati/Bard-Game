800bcf0b4691769677ab5001d6bc9626fdacb784
Vi Vailati <vi.vailati.845@gmail.com>
2025-06-14 19:55:46 -0600

    Add animation playing logic for player

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -9,18 +9,23 @@ public partial class Player : CharacterBody2D
     [Export]
     public int Gravity { get; set; } = 200;
 
+    [Export]
+    public int JumpHeight { get; set; } = -100;
+
     public override void _PhysicsProcess(double delta)
     {
         var velocity = Velocity;
         velocity.Y += Gravity * (float) delta;
         Velocity = velocity;
 
-        HorizontalMovement();
+        CalculateHorizontalMovement();
 
-        MoveAndSlide();
+        // MoveAndSlide();
+
+        PlayPlayerAnimations();
     }
 
-    private void HorizontalMovement()
+    private void CalculateHorizontalMovement()
     {
         var horizontalInput = Input.GetActionStrength("ui_right") - Input.GetActionStrength("ui_left");
 
@@ -28,5 +33,27 @@ public partial class Player : CharacterBody2D
         vel.X = horizontalInput * Speed;
 
         Velocity = vel;
+    }
+
+    private void PlayPlayerAnimations()
+    {
+        var animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+
+        if (Input.IsActionPressed("ui_left") || Input.IsActionJustReleased("ui_jump"))
+        {
+            animatedSprite.FlipH = true;
+            animatedSprite.Play("run");
+        }
+
+        if (Input.IsActionPressed("ui_right") || Input.IsActionJustReleased("ui_jump"))
+        {
+            animatedSprite.FlipH = false;
+            animatedSprite.Play("run");
+        }
+
+        if (!Input.IsAnythingPressed())
+        {
+            animatedSprite.Play("idle");
+        }
     }
 }
